b6e2a98de4b4ef2366bbd3967be91eb9f98e46b3
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-06-18 14:08:37 +0900

    remove : unused files

--- src/routers/index.ts
+++
@@ -1,4 +0,0 @@
-import loadable from "@loadable/component";
-
-export const SellerRouter = loadable(() => import("./sellerRouter"));
-export const UserRouter = loadable(() => import("./userRouter"));
