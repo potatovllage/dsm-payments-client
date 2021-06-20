ced98f20cb23eb3494b96dc608ca207e48788e4f
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-06-20 23:47:24 +0900

    update(assets) : add double arrow button

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,4 @@
 export { BiLockAlt, BiBadge, BiAbacus, BiAdjust } from "react-icons/bi";
+export { BsChevronDoubleLeft, BsChevronDoubleRight } from "react-icons/bs";
 export { ImCoinDollar } from "react-icons/im";
 export { default as dsmStudentUnion } from "./dsm-student-union.png";
