24a17540005bd6743bbd501446153b71cf1dc12c
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-06-21 16:06:47 +0900

    update(assets) : navigator logo

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,5 @@
 export { BiLockAlt, BiBadge, BiAbacus, BiAdjust } from "react-icons/bi";
-export { BsChevronDoubleLeft, BsChevronDoubleRight } from "react-icons/bs";
 export { ImCoinDollar } from "react-icons/im";
+export { IoQrCodeOutline, IoQrCode } from "react-icons/io5";
+export { RiCoupon2Line, RiCoupon2Fill } from "react-icons/ri";
 export { default as dsmStudentUnion } from "./dsm-student-union.png";
