e2a0f47bffc68393dd0a2fea7163aedeccaab628
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-06-27 23:14:35 +0900

    add : withNeedAuth HOC

---
+++ src/components/main/withNeedAuth.tsx
@@ -0,0 +1,18 @@
+import { ComponentType } from "react";
+import { Redirect } from "react-router-dom";
+
+const withNeedAuth = <Props extends {}>(Component: ComponentType<Props>) => {
+  return (params: Props) => {
+    try {
+      const accessToken = localStorage.getItem("accessToken");
+
+      if (!accessToken) throw new Error("Cannot find access token");
+
+      return <Component {...params} />;
+    } catch {
+      return <Redirect to="/login" />;
+    }
+  };
+};
+
+export default withNeedAuth;
