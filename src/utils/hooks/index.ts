24f1e59dd094601291f90c7301d757e266f5a0de
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-06-27 23:14:52 +0900

    add : barrel file

---
+++ src/utils/hooks/index.ts
@@ -0,0 +1,3 @@
+export { default as useMovePage } from "./useMovePage";
+export { default as useSocket } from "./useSocket";
+export * from "./public";
---
+++ src/utils/hooks/public/index.ts
@@ -0,0 +1,4 @@
+export { default as useLoading } from "./useLoading";
+export { default as useResize } from "./useResize";
+export { default as useToggle } from "./useToggle";
+export { default as useValue } from "./useValue";
