3927c461b10471669c02680a701e22e3ac6b111a
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-06-27 23:11:44 +0900

    add : ghost house badge

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,5 @@
-export { BiLockAlt, BiBadge, BiAbacus, BiAdjust } from "react-icons/bi";
 export { ImCoinDollar } from "react-icons/im";
 export { IoQrCodeOutline, IoQrCode } from "react-icons/io5";
 export { RiCoupon2Line, RiCoupon2Fill } from "react-icons/ri";
-export { default as dsmStudentUnion } from "./dsm-student-union.png";
+
+export { default as badgeGhostHouse } from "./bg.webp";
