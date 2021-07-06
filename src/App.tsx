eb1f5be536ab6626c6cd0a51c2681b3653493f82
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-07-06 14:45:19 +0900

    update : 다 보인다 했제

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 
 import { Main, Login, TeacherLogin } from "./components";
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Switch>
-        <Route path="/teacher/login" component={TeacherLogin} />
-        <Route path="/login" component={Login} />
-        <Route path="/" component={Main} />
+        <Route exact path="/teacher/login" component={TeacherLogin} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/" component={Main} />
+        <Route
+          component={() => {
+            const daBoindaHaetje =
+              "https://www.youtube.com/watch?v=DDYJysAYVWo";
+
+            window.location.href = daBoindaHaetje;
+
+            return null;
+          }}
+        />
       </Switch>
-    </>
+    </BrowserRouter>
   );
 };
 
